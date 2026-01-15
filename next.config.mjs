89d256f90f070b9f2da3e68bc9e50ca9a0cef26b
samuelwaiweng.yong <samuelyongw@gmail.com>
2026-01-15 16:23:48 +0900

    adjusting config to solve Error: ENOENT: no such file or directory, open '/vercel/path0/.next/server/middleware.js.nft.json'

--- next.config.mjs
+++ next.config.mjs
@@ -24,9 +24,6 @@ const nextConfig = {
       },
     ],
   },
-  experimental: {
-    viewTransition: true,
-  },
   sassOptions: {
     compiler: "modern",
     silenceDeprecations: ["legacy-js-api"],
